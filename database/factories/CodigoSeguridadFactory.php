8e1eca9b966aee5497b879b10562e836604040fc
edgpin16 <edgardo.pinto16@gmail.com>
2026-03-08 21:59:54 -0400

    refactor: cambiando los codigos de seguridad en el factory

--- database/factories/CodigoSeguridadFactory.php
+++ database/factories/CodigoSeguridadFactory.php
@@ -3,15 +3,25 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class CodigoSeguridadFactory extends Factory
 {
     public function definition(): array
     {
         return [
-            'hash_code' => Str::random(10),
+            'hash_code' => $this->generarHashCode(),
             'fecha_expiracion' => fake()->dateTimeBetween('now', '+6 months')->format('Y-m-d'),
         ];
+    }
+
+    private function generarHashCode(): string
+    {
+        $partes = [
+            'MUS',
+            strtoupper(substr(bin2hex(random_bytes(2)), 0, 4)),
+            strtoupper(substr(bin2hex(random_bytes(2)), 0, 4)),
+            strtoupper(substr(bin2hex(random_bytes(2)), 0, 4))
+        ];
+        return implode('-', $partes);
     }
 }
